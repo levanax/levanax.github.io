50a355850d6a98c9aa6662a9f9a1a2d01ac05e01
Levan <levana.xue@hotmail.com>
2018-09-30 14:07:12 +0800

    update post toc 显示问题

--- themes/pond/source/js/post.js
+++ themes/pond/source/js/post.js
@@ -1,26 +1,26 @@
-(function($){
-    $.when( $.ready ).then(function() {
-      //文章目录固定
-      var tocDom = $('#postToc');
-      if(tocDom){
-          var top = $('nav').height();
+(function($) {
+  $.when($.ready).then(function() {
+    //文章目录固定
+    var tocDom = $('#postToc');
+    if (tocDom) {
+      var top = $('nav').height();
 
-          tocDom.pushpin({
-            top:top,
-            buttom: 10,
-            offset: 0
-          });
-          //tocDom.css('width', tocDom.parent().width());
-      }
-  
-      var postContentDom = $('#postContent');
-      if(postContentDom){
-          // $('#postContent h1').scrollSpy();
-          // $('#postContent h2').scrollSpy();
-          // $('#postContent h3').scrollSpy();
-          // $('#postContent h4').scrollSpy();
-          // $('#postContent h5').scrollSpy();
-      }
-  
-    })
-  })(jQuery);
+      tocDom.pushpin({
+        top: top,
+        right: 0,
+        // buttom: 10,
+        offset: 0
+      });
+      //tocDom.css('width', tocDom.parent().width());
+    }
+
+    var postContentDom = $('#postContent');
+    if (postContentDom) {
+      // $('#postContent h1').scrollSpy();
+      // $('#postContent h2').scrollSpy();
+      // $('#postContent h3').scrollSpy();
+      // $('#postContent h4').scrollSpy();
+      // $('#postContent h5').scrollSpy();
+    }
+  });
+})(jQuery);
