f5688b04412a3429305e5364cb8acf7054f0bd3a
Admin <levana.xue@qq.com>
2019-08-18 16:47:35 +0800

    UPDATE STYLE

--- themes/pond/source/js/main.js
+++ themes/pond/source/js/main.js
@@ -1,38 +1,50 @@
-;(function() {
+(function() {
   $.when($.ready).then(function() {
-    loadBackground()
-    $('.page-progress').hide();
-  })
+    loadBackground(function() {
+      setTimeout(function() {
+        $(".page-progress").hide();
+      }, 500);
+    }, this);
+  });
 
-  function loadBackground() {
+  function loadBackground(callback, scope) {
     //取0 几率小
-    var imgList = ['bgr_1.jpg']
-    var load_img_i = 0
+    var imgList = ["bgr_1.jpg"];
+    var load_img_i = 0;
     if (imgList.length > 1) {
-      load_img_i = Math.ceil(Math.random() * imgList.length - 1)
+      load_img_i = Math.ceil(Math.random() * imgList.length - 1);
     }
-    var divE = document.createElement('div')
-    divE.setAttribute('id', 'main-bgr')
-    var innerDiv = document.createElement('div')
-    innerDiv.setAttribute('style', 'height:100%;background: rgba(0,0,0,.4);')
-    divE.appendChild(innerDiv)
 
-    $('body').append(divE)
+    var imgSrc = "/imgs/bgr/" + imgList[load_img_i];
+    var img = new Image();
+    img.onload = function() {
+      callback.call(scope);
+    };
+    img.src = imgSrc;
 
     var isIE = false;
-    try{
+    try {
       isIE = !!window.ActiveXObject || "ActiveXObject" in window;
-    }catch(e){console.error(e)}
-    if(isIE){
-      $('body').attr(
-        'style',
-        ['background-size:100% 100%;background-attachment: fixed;background-position: center top;background-image:url(/imgs/bgr/', imgList[load_img_i], ')'].join('')
-      )
-    }else{
-      $('#main-bgr').attr(
-        'style',
-        ['background-size:100% 100%;background-attachment: fixed;background-position: center top;background-image:url(/imgs/bgr/', imgList[load_img_i], ')'].join('')
-      )
+    } catch (e) {
+      console.error(e);
+    }
+    var styleStr = [
+      "background-size:100% 100%;background-attachment: fixed;background-position: center top;background-image:url(",
+      imgSrc,
+      ")"
+    ].join("");
+
+    if (isIE) {
+      $("body").attr("style", styleStr);
+    } else {
+      var divE = document.createElement("div");
+      divE.setAttribute("id", "main-bgr");
+      var innerDiv = document.createElement("div");
+      innerDiv.setAttribute("style", "height:100%;background: rgba(0,0,0,.4);");
+      divE.appendChild(innerDiv);
+
+      $("body").append(divE);
+      $("#main-bgr").attr("style", styleStr);
     }
   }
-})()
+})();
